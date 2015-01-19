2094d6e350cf7aa6ee24f92c66ed542947467d4e
Rishab Arora <ra.rishab@gmail.com>
2015-01-19 21:49:41 +0530

    Allow upload of readings

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
 require 'sinatra'
+require 'haml'
 
 class MonitorIOT < Sinatra::Base
   get '/healthcheck' do
     "Hi there!"
   end
 
+  get '/upload' do
+    haml :upload
+  end
+
+  post "/:id/upload" do 
+    File.open("uploads/#{params[:id]}.jpg", "w") do |f|
+      f.write(params['reading'][:tempfile].read)
+    end
+    return "Reading uploaded"
+  end
 end
+
